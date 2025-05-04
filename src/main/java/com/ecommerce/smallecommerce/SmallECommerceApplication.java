ede9e2cadfa231c99d41517948d627a7b8550b89
Ant0On <antoni.cieslak0@gmail.com>
2025-05-04 13:30:55 +0200

    initial commit

---
+++ src/main/java/com/ecommerce/smallecommerce/SmallECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.smallecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/smallecommerce/SmallECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.smallecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
